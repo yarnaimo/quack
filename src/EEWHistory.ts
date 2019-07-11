cc666cb9f61a20741fb7cf834a1a60dda0ff29b2
yamaimo <yarnaimo@gmail.com>
2019-07-11 14:11:20 +0900

    Update EEWHistory.ts

--- src/EEWHistory.ts
+++ src/EEWHistory.ts
@@ -1,5 +1,35 @@
 import { IEEWData } from './EEWData'
 
+const LOG_EXPIRATION = 1000 * 60 * 15 // ms
+
+export const EEWHistory = () => {
+    const logs = new Map<string, string[]>()
+
+    const clearAfterExpiration = (report_id: string) => {
+        setTimeout(() => logs.delete(report_id), LOG_EXPIRATION)
+    }
+
+    const getRef = ({ report_id }: IEEWData) => {
+        if (!logs.has(report_id)) {
+            logs.set(report_id, [])
+            clearAfterExpiration(report_id)
+        }
+        return logs.get(report_id)
+    }
+
+    const process = (data: IEEWData) => {
+        const ref = getRef(data)
+        const intensityChanged = ref[ref.length - 1] !== data.calcintensity
+
+        if (intensityChanged) {
+            ref.push(data.calcintensity)
+        }
+        return { intensityChanged }
+    }
+
+    return { process }
+}
+
 export class EEWHistory {
     static logs = {} as {
         [id: string]: string[]
